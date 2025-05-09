d585f8f1ea1dd28c44d4c828b77cf755f4ed79b3
telkraft <can@telkraft.com>
2025-05-09 21:15:59 +0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function setLanguage(lang) {
-  const elements = document.querySelectorAll('[data-tr]');
-  elements.forEach(el => {
-    const text = el.getAttribute(`data-${lang}`);
-    if (text) {
-      el.textContent = text;
-    }
-  });
-}
